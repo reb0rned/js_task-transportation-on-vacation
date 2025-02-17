29c18ee3203a74af0dd21aea415f2cbb033b6749
reb0rned <affmarket77@gmail.com>
2025-02-17 21:16:45 +0200

    minor fixes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,22 +3,23 @@
  *
  * @return {number}
  */
+
+const PRICE_PER_DAY = 40;
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 20;
+
 function calculateRentalCost(days) {
   // write code here
-  const PRICE_PER_DAY = 40;
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-  const SHORT_TERM = 3;
-  const SHORT_TERM_DISCOUNT = 20;
-
   const basePrice = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
-    return days * PRICE_PER_DAY - LONG_TERM_DISCOUNT;
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    return days * PRICE_PER_DAY - SHORT_TERM_DISCOUNT;
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
   return basePrice;
